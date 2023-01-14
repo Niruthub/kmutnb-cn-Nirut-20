74146224f2daf99f57e956580ce7fcbd15cac6f9
Niruthub <s6303051623055@email.kmutnb.ac.th>
2023-01-14 21:53:32 +0700

    save-lab03-v2

--- lab03-2023-01-13/as-01/src/main.rs
+++ lab03-2023-01-13/as-01/src/main.rs
@@ -39,7 +39,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_count_unique_work() {
+    fn sum_of_number() {
         let s = "this cat this bat this rat";
         let result: Vec<&str> = s.split(' ').collect();
         let result = step2(result);
